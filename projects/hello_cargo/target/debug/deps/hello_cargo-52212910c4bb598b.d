94ce1937bdf5cffa873e0627377d6446b1def92c
D-Guy <ddcool535@gmail.com>
2025-10-07 21:40:56 -0400

    Fixed gitignore oops

--- projects/hello_cargo/target/debug/deps/hello_cargo-52212910c4bb598b.d
+++
@@ -1,5 +0,0 @@
-C:\Users\dguy\codingStuff\learn-rs\projects\hello_cargo\target\debug\deps\hello_cargo-52212910c4bb598b.d: src\main.rs
-
-C:\Users\dguy\codingStuff\learn-rs\projects\hello_cargo\target\debug\deps\libhello_cargo-52212910c4bb598b.rmeta: src\main.rs
-
-src\main.rs:
--- projects/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-C:\Users\dguy\codingStuff\learn-rs\projects\hello_cargo\target\debug\hello_cargo.exe: C:\Users\dguy\codingStuff\learn-rs\projects\hello_cargo\src\main.rs
