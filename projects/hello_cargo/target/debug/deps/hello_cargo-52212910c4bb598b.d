ae0ffda49be668f85016c6d885f604e451cf9702
D-Guy <ddcool535@gmail.com>
2025-10-07 21:37:52 -0400

    Hello world + pre-dependencies

---
+++ projects/hello_cargo/target/debug/deps/hello_cargo-52212910c4bb598b.d
@@ -0,0 +1,5 @@
+C:\Users\dguy\codingStuff\learn-rs\projects\hello_cargo\target\debug\deps\hello_cargo-52212910c4bb598b.d: src\main.rs
+
+C:\Users\dguy\codingStuff\learn-rs\projects\hello_cargo\target\debug\deps\libhello_cargo-52212910c4bb598b.rmeta: src\main.rs
+
+src\main.rs:
---
+++ projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\dguy\codingStuff\learn-rs\projects\hello_cargo\target\debug\hello_cargo.exe: C:\Users\dguy\codingStuff\learn-rs\projects\hello_cargo\src\main.rs
