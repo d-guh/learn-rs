e9a5b8ca602dabf17a56f03eb3fbaf382685f229
D-Guy <dguy@dguy.net>
2026-03-03 21:29:59 -0500

    Completed quiz2

--- rustlings/exercises/quizzes/quiz2.rs
+++ rustlings/exercises/quizzes/quiz2.rs
@@ -27,7 +27,18 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output: Vec<String> = Vec::new();
+        for (string, command) in input {
+            let result = match command {
+                Command::Uppercase => string.to_ascii_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                Command::Append(num) => string + &"bar".repeat(num),
+            };
+            output.push(result);
+        }
+        output
+    }
 }
 
 fn main() {
@@ -37,7 +48,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
