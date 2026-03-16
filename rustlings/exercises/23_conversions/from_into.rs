559e321eb6af83ec0249ec6217c6e5f95e58265b
D-Guy <dguy@dguy.net>
2026-03-16 12:19:37 -0400

    Completed 23 - Done!

--- rustlings/exercises/23_conversions/from_into.rs
+++ rustlings/exercises/23_conversions/from_into.rs
@@ -34,7 +34,26 @@ impl Default for Person {
 // 5. Parse the second element from the split operation into a `u8` as the age.
 // 6. If parsing the age fails, return the default of `Person`.
 impl From<&str> for Person {
-    fn from(s: &str) -> Self {}
+    fn from(s: &str) -> Self {
+        let parts: Vec<&str> = s.split(",").collect();
+
+        if parts.len() != 2 {
+            return Self::default();
+        }
+
+        let name: String = parts[0].to_string();
+
+        if name.is_empty() {
+            return Self::default();
+        }
+
+        let age: u8 = match parts[1].parse::<u8>() {
+            Ok(num) => num,
+            Err(_) => return Self::default(),
+        };
+
+        Person { name, age }
+    }
 }
 
 fn main() {
