93c13d17189f51b3b41b67e13e5255087ab72804
D-Guy <dguy@dguy.net>
2026-08-05 20:06:25 -0400

    complete 12.3 notes, WIP minigrep - lib

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -3,6 +3,8 @@ use std::fs;
 use std::process;
 use std::error::Error;
 
+use minigrep::search;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -11,21 +13,10 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for '{}'", config.query);
-    println!("In file '{}'", config.file_path);
-
     if let Err(e) = run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
 }
 
 struct Config {
@@ -44,4 +35,14 @@ impl Config {
 
         Ok(Config { query, file_path })
     }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
+    Ok(())
 }
